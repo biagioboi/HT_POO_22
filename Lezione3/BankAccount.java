60663d76531e819dd4397d211f5f6111c54df8ff
biagioboi <biagioboi@gmail.com>
2022-12-08 11:31:55 +0100

    Aggiunto materiale per Lezione3

---
+++ Lezione3/BankAccount.java
@@ -0,0 +1,35 @@
+package htoop22.Bank;
+
+import java.io.Serializable;
+
+public class BankAccount implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private double saldo;
+    private Cliente cliente;
+
+    public BankAccount(double saldo, Cliente cliente) {
+        this.saldo = saldo;
+        this.cliente = cliente;
+    }
+
+    /**
+     * @param amount the amount of money to withdrawal
+     * @throws NotEnoughMoneyException if there are no enough money
+     */
+    public void withdrawal(double amount) throws NotEnoughMoneyException{
+        if (amount > this.saldo) {
+            throw new NotEnoughMoneyException("Non ci sono abbastanza soldi");
+        } else {
+            this.saldo -= amount;
+        }
+    }
+
+    /**
+     * @param amount the amount of money to deposit
+     */
+    public void deposit(double amount) {
+        this.saldo += amount;
+    }
+}
---
+++ Lezione3/Cliente.java
@@ -0,0 +1,70 @@
+package htoop22.Bank;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String nome;
+    private String cognome;
+    private final int customerNumber;
+    private final int customerPin;
+    private ArrayList<BankAccount> conti;
+
+    public Cliente(String nome, String cognome, int aNumber, int aPin, ArrayList<BankAccount> conti) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.customerNumber = aNumber;
+        this.customerPin = aPin;
+        this.conti = conti;
+    }
+
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public int getCustomerPin() {
+        return customerPin;
+    }
+
+
+    public void addContoToCliente(BankAccount conto) {
+        this.conti.add(conto);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public ArrayList<BankAccount> getConti() {
+        return conti;
+    }
+
+    public void setConti(ArrayList<BankAccount> conti) {
+        this.conti = conti;
+    }
+
+    /**
+     Tests if this customer matches a customer number and PIN.
+     @param aNumber a customer number
+     @param aPin a personal identification number
+     @return true if the customer number and PIN match
+     */
+    public boolean match(int aNumber, int aPin) {
+        return this.customerNumber == aNumber && this.customerPin == aPin;
+    }
+}
