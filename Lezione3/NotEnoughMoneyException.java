4cea180b7b0b6be7edfbc42d41f1ddc65dd7defd
Biagio Boi <biagioboi@gmail.com>
2022-12-14 09:29:27 +0100

    Lezione del 13 Dic

--- Lezione3/NotEnoughMoneyException.java
+++
@@ -1,7 +0,0 @@
-package htoop22.Bank;
-
-public class NotEnoughMoneyException extends IllegalArgumentException{
-    public NotEnoughMoneyException(String message) {
-        super(message);
-    }
-}
